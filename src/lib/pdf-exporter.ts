3376ef48554d65c5e61dd67f5b5e8a1c08f8c034
duber-parra <dubergames@gmail.com>
2025-08-15 02:05:32 -0500

    Revert "Use ellipsis overflow in PDF tables"

--- src/lib/pdf-exporter.ts
+++ src/lib/pdf-exporter.ts
@@ -11,7 +11,7 @@
             cellPadding: 1,
             lineColor: [200,200,200],
             lineWidth: 0.25,
-            overflow: 'ellipsize', // prevent multi-line cells
+            overflow: 'linebreak',
             tableWidth: 'wrap'
         },
         headStyles: {
